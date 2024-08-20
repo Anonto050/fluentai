1fd99a5b5c861755f9b9e8417f6746bd6006221c
BRAINIAC2677 <brainiac2677@gmail.com>
2024-08-20 11:32:34 +0600

    fixing secret hide bug 2

--- src/test/java/ai/fluent/fluentai/FluentaiApplicationTests.java
+++ src/test/java/ai/fluent/fluentai/FluentaiApplicationTests.java
@@ -8,6 +8,7 @@ class FluentaiApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 		// print environment variables
 		System.out.println("Environment variables:");
 		System.getenv().forEach((k, v) -> System.out.println(k + ": " + v));
